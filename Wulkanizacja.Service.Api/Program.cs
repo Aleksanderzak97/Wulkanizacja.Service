bc4f45ce644414fa81217198997e366b35ba5c07
Aleksander <aleksanderzak97@gmail.com>
2025-02-10 16:48:16 +0100

    Początki serwisu

---
+++ Wulkanizacja.Service.Api/Program.cs
@@ -0,0 +1,24 @@
+﻿using Convey;
+using Convey.Logging;
+using Convey.WebApi;
+using Convey.WebApi.CQRS;
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Konfiguracja Convey
+builder.Services
+    .AddConvey()
+    .AddWebApi()
+    .Build();
+
+var app = builder.Build();
+
+// Middleware
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
