94237c1ec1b680dd86655428dcb89a6e21a9c103
Aleksander <aleksanderzak97@gmail.com>
2025-02-11 18:17:40 +0100

    Dodanie atrybutów

---
+++ Wulkanizacja.Service.Application/Commands/Attributes/RetryAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wulkanizacja.Service.Application.Commands.Attributes
+{
+    internal class RetryAttribute
+    {
+    }
+}
